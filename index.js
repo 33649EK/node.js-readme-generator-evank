e2ef36140460e6628de3401f99c7903765a1a14f
Evan Keller <33649ek@gmail.com>
2023-12-18 12:36:00 -0700

    added more licenses to choose from

--- index.js
+++ index.js
@@ -37,7 +37,17 @@ inquirer
       type: "list",
       message: "Please choose a license for your project.",
       name: "license",
-      choices: ["Apache", "MIT", "ISC", "GNU GPLv3", "None"],
+      choices: [
+        "Apache",
+        "MIT",
+        "ISC",
+        "GNU GPLv3",
+        "BSD 3-Clause",
+        "BSD 2-Clause",
+        "Mozilla",
+        "Creative Commons",
+        "The Unlicense",
+      ],
     },
     {
       type: "input",
@@ -52,7 +62,7 @@ inquirer
   ])
   .then((userInputs) =>
     fs.writeFile("README.md", createMarkdown(userInputs), (err) =>
-      err ? console.log(err) : console.log("Success!")
+      err ? console.log(err) : console.log(userInputs, "Success!")
     )
   );
 
@@ -73,10 +83,15 @@ const createMarkdown = ({
 
   ## Table of Contents
   [Installation](#installation)
+
   [Usage](#usage)
+
   [Credits](#credits)
+
   [Tests](#tests)
+
   [License](#license)
+  
   [Questions](#questions)
 
   ## Installation
@@ -91,9 +106,9 @@ const createMarkdown = ({
   ## License
   This application is using the ${license} license.
   ## Questions
-  Please reach out to me with any questions that you may have.
+  My GitHub project
 
-  GitHub Username: ${github}
+  GitHub: [${github}](https://github.com/${github})
 
   Email: ${email}
   `;
@@ -108,7 +123,15 @@ const licenseBadge = (license) => {
       return "[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)]";
     case "GNU GPLv3":
       return "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)]";
-    default:
-      return "";
+    case "BSD 3-Clause":
+      return "[![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)]";
+    case "BSD 2-Clause":
+      return "[![License](https://img.shields.io/badge/License-BSD%202--Clause-orange.svg)]";
+    case "Mozilla":
+      return "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)]";
+    case "Creative Commons":
+      return "[![License: CC0-1.0](https://licensebuttons.net/l/zero/1.0/80x15.png)]";
+    case "The Unlicense":
+      return "[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)]";
   }
 };
